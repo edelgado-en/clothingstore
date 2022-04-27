85816850177c4064291c57066048df696d3403bf
edelgado-git <61290256+edelgado-git@users.noreply.github.com>
2022-04-26 20:50:18 -0400

    testing ggpush

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,7 @@
 
 /**
  * 
- * @param {*} state the redux store state test 
+ * @param {*} state the redux store state 
  * @returns the categories map from the categories reducer in the redux store
  */
 export const getCategoriesMap = (state) => state.categories.categoriesMap;
