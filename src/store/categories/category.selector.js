b194a66c2e19755354de054eadd36ca51f46d4f9
edelgado-git <61290256+edelgado-git@users.noreply.github.com>
2022-04-26 20:43:16 -0400

    test credentials

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,7 @@
 
 /**
  * 
- * @param {*} state the redux store state 
+ * @param {*} state the redux store state test 
  * @returns the categories map from the categories reducer in the redux store
  */
 export const getCategoriesMap = (state) => state.categories.categoriesMap;
