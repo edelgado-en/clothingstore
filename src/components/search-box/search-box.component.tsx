07b373e1e8ac609cb873c37356f208c88e6b3611
edelgado-git <61290256+edelgado-git@users.noreply.github.com>
2022-04-29 08:59:36 -0400

    added jsx to tsconfig

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,1 +1,20 @@
 const testName: string = 'test';
+
+type SearchBoxProps = {
+    className: string;
+    placeholder?: string;
+    onChangeHandler: (parameter: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => {
+   
+        <input 
+            type="text"
+            className={`search-box ${className}`}
+            placeholder={placeholder}
+            onChange={onChangeHandler}
+        />
+   
+}
+
+export default SearchBox;
